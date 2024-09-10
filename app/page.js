29277e935ab4f6e6d6e0c71acede244ac3a2b3a5
Yosef Haimjan <yosef050468@gmail.com>
2024-09-10 14:42:50 +0300

    Add navbar comp & update main app

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-7xl">Home Page</h1>
-      <Link href="/about" className="text-2xl">
-        about page
+      <h1 className="text-5xl font-bold mb-8">Next.js Tutorial</h1>
+      <Link href="/client" className="btn btn-accent">
+        GET STARTED
       </Link>
     </div>
   );
---
+++ components/Navbar.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+
+const links = [
+  { href: "/client", label: "Client" },
+  { href: "/drinks", label: "Drinks" },
+  { href: "/tasks", label: "Tasks" },
+  { href: "/query", label: "Query" },
+];
+const Navbar = () => {
+  return (
+    <nav className="bg-base-300 py-4 ">
+      <div className="navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row">
+        <Link href="/" className="btn btn-error font-bold ">
+          NEXTJS
+        </Link>
+        <ul className="menu menu-horizontal md:ml-8">
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href} className="capitalize text-black-500 px-7 mx-3">
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
