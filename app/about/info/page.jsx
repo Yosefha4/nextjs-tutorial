bf32a8852a59dadcba35d07442ed572bddd1e4fe
Yosef Haimjan <yosef050468@gmail.com>
2024-09-10 14:42:15 +0300

    Add new pages

---
+++ app/about/info/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutInfo = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">AboutInfo Page</h1>
+    </div>
+  );
+};
+
+export default AboutInfo;
---
+++ app/tasks/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TasksPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">Tasks Page</h1>
+    </div>
+  );
+};
+
+export default TasksPage;
