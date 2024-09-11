217952d701a1c0c4ef9af28061efee4d3abf14bc
Yosef Haimjan <yosef050468@gmail.com>
2024-09-11 11:30:56 +0300

    error page

--- app/drinks/error.jsx
+++ app/drinks/error.jsx
@@ -1,8 +1,9 @@
 "use client";
 const error = (error) => {
-  console.log(error);
+  console.log(error.message);
   return (
     <div className="flex flex-col">
+      {/* <p className="text-4xl font text-center bg-red-500">Failed to fetch drinks.</p> */}
       <p className="text-4xl font text-center">{error.error.message}</p>
     </div>
   );
