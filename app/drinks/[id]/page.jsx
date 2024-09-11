7c5a172dacd41244bd33182101cabebb0ef97cff
Yosef Haimjan <yosef050468@gmail.com>
2024-09-11 13:37:22 +0300

    Remote images

--- app/drinks/[id]/page.jsx
+++ app/drinks/[id]/page.jsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import Drink1 from "./randomDrink1.jpg";
 
-console.log(Drink1);
-
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const fetchSingleDrink = async (id) => {
@@ -19,15 +17,13 @@ const DrinkItem = async ({ params }) => {
   const title = data?.drinks[0].strDrink;
   const imgSrc = data?.drinks[0].strDrinkThumb;
 
-  console.log(title, imgSrc);
-
   return (
     <div>
       <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
         Back to drinks
       </Link>
-
-      <Image src={Drink1} alt={title} className="w-48 h-48 rounded" />
+      <Image src={imgSrc} width={300} height={300} className="w-48 j-48 rounded-lg shadow-lg mb-4 " priority alt={title} />
+      {/* <Image src={Drink1} alt={title} className="w-48 h-48 rounded" /> */}
       <h1 className="text-4xl mb-8">{title}</h1>
     </div>
   );
