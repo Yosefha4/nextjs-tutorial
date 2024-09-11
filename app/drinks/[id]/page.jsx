c86efe9efffadb965f29fcca9a15131c71c66a38
Yosef Haimjan <yosef050468@gmail.com>
2024-09-11 12:13:50 +0300

    single drink challenge

--- app/drinks/[id]/page.jsx
+++ app/drinks/[id]/page.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const DrinkItem = ({ params }) => {
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+const fetchSingleDrink = async (id) => {
+  const response = await fetch(`${url}${id}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch single drink.");
+  }
+  return response.json();
+};
+
+const DrinkItem = async ({ params }) => {
+  const data = await fetchSingleDrink(params.id);
+  const title = data?.drinks[0].strDrink;
+  const imgSrc = data?.drinks[0].strDrinkThumb;
+
+  console.log(title, imgSrc);
+
   return (
     <div>
-      <h1 className="text-7xl">{params.id}</h1>
+      <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
+        Back to drinks
+      </Link>
+      <h1 className="text-4xl mb-8">{title}</h1>
+      {/* <Image src={imgSrc} alt={title} width={300} height={300} /> */}
     </div>
   );
 };
