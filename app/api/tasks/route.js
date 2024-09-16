3ee191eed94f8b03b86c702322502e4ad56a384f
Yosef Haimjan <yosef050468@gmail.com>
2024-09-16 08:58:08 +0300

    POST action

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,7 +1,18 @@
 import prisma from "@/utils/db";
+import { NextResponse } from "next/server";
 
-export const GET = async (req) => {
+export const GET = async (request) => {
   const tasks = await prisma.task.findMany();
 
   return Response.json({ data: tasks });
+};
+
+export const POST = async (request) => {
+  const data = await request.json();
+  const task = await prisma.task.create({
+    data: {
+      content: data.content,
+    },
+  });
+  return NextResponse.json({ data: task });
 };
